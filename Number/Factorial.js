53d50981b68e1b582a738bc0def7f3dcd6f3989b
Mohamed Abraham CHERIF <cherifmohamedabraham@gmail.com>
2024-06-23 02:49:16 +0000

    Update Factorial.js

--- Number/Factorial.js
+++ Number/Factorial.js
@@ -1,18 +1,24 @@
-function calculerFactorielle(nombre) {
+// Fonction pour calculer la factorielle d'un nombre
+const calculerFactorielle = (nombre) => {
     // Vérifier si le nombre est négatif
     if (nombre < 0) {
         return "Impossible";
     }
-    if (nombre === 0 || nombre === 1) {  //Si le nombre est 0 ou 1
-        return 'Le factoriel de ' + nombre + ' est : 1';
+
+    // Si le nombre est 0 ou 1, retourner 1
+    if (nombre === 0 || nombre === 1) {
+        return `Le factoriel de ${nombre} est : 1`;
     } else {
-        resultat=1;
-        for (i = 1; i <= nombre; i++) {  //Si le nombre est sup a 1
+        let resultat = 1;
+
+        // Utiliser une boucle for pour calculer la factorielle
+        for (let i = 1; i <= nombre; i++) {
             resultat *= i;
         }
-        return 'Le factoriel de ' + nombre + ' est : ' + resultat;
+        return `Le factoriel de ${nombre} est : ${resultat}`;
     }
-}
+};
+
 //Exemple :
 console.log(calculerFactorielle(9));
 console.log(calculerFactorielle(3));
