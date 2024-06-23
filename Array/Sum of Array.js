c8d72ed4c6ac98c06a14f54a517b202b8512389e
Mohamed Abraham CHERIF <cherifmohamedabraham@gmail.com>
2024-06-23 02:47:23 +0000

    Update Sum of Array.js

--- Array/Sum of Array.js
+++ Array/Sum of Array.js
@@ -1,9 +1,14 @@
-function sommeTableau(tableau) {
-    somme = 0;
-    for (nombre = 0; nombre < tableau.length; nombre++) {
+// Fonction pour calculer la somme des éléments d'un tableau
+const sommeTableau = (tableau) => {
+    // Initialisqtion de la somme à 0
+    let somme = 0;
+
+    // Utilisqtion d'une boucle for pour parcourir chaque élément du tableau
+    for (let nombre = 0; nombre < tableau.length; nombre++) {
         somme += tableau[nombre];
     }
-    return 'La somme est : ' + somme;
-}
+    return `La somme est : ${somme}`;
+};
+
 //Exemple:
 console.log(sommeTableau([1, 20, 3, 4, 5, 6, 7, 8, 9, 10]));
