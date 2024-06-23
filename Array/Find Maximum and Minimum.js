4e5001a98a4e87c6997b45fa6dc3144d7963ca18
Mohamed Abraham CHERIF <cherifmohamedabraham@gmail.com>
2024-06-23 02:44:05 +0000

    Update Find Maximum and Minimum.js

--- Array/Find Maximum and Minimum.js
+++ Array/Find Maximum and Minimum.js
@@ -1,29 +1,31 @@
-//Fonction Find Maximum
-function findMax(num) {
-    maxValue = num[0];
+// Fonction pour trouver le nombre maximum dans un tableau
+const findMax = (num) => {
+    let maxValue = num[0];
 
-    for (index = 1; index < num.length; index++) {
+    for (let index = 1; index < num.length; index++) {
         if (num[index] > maxValue) {
             maxValue = num[index];
         }
     }
-    return 'Le nombre maximum est ' + maxValue;
-}
+    return `Le nombre maximum est ${maxValue}`;
+};
+
 //Exemple:
 console.log(findMax([1, 20, 3, 4, 5, 6, 7, 8, 9, 10]));
 
 
-//Fonction Find Minimum
-function findMin(num) {
-    minValue = num[0];
+// Fonction pour trouver le nombre minimum dans un tableau
+const findMin = (num) => {
+    let minValue = num[0];
 
-    for (index = 1; index < num.length; index++) {
+    for (let index = 1; index < num.length; index++) {
         if (num[index] < minValue) {
             minValue = num[index];
         }
     }
-    return 'Le nombre minimum est ' + minValue;
-}
+    return `Le nombre minimum est ${minValue}`;
+};
+
 //Exemple:
 console.log(findMin([1, 20, 3, 4, 5, 6, 7, 8, 9, 10]));
 
