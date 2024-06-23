3667f0cd29774778724d5af5a3d9d2ba95ce58c9
Mohamed Abraham CHERIF <cherifmohamedabraham@gmail.com>
2024-06-23 03:11:46 +0000

    Update Reverse a String.js

--- String/Reverse a String.js
+++ String/Reverse a String.js
@@ -1,10 +1,10 @@
-function inverses(chaine){
-	var chaineInverse = '';
+const inverses = (chaine) => {
+    let chaineInverse = '';
 
-	for (index = chaine.length - 1; index >= 0; index--){
-		chaineInverse = chaineInverse + chaine[index];
-	}
-	return 'L inverse de ' + chaine + ' est : ' + chaineInverse;
-}
+    for (let index = chaine.length - 1; index >= 0; index--) {
+        chaineInverse += chaine[index];
+    }
+    return `L'inverse de ${chaine} est : ${chaineInverse}`;
+};
 //Exemple :
 console.log(inverses('MOHAMED'))
