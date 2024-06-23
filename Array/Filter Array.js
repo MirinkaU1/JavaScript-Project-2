af73628baf6eef04dfedae1faa702ef3770a0974
Mohamed Abraham CHERIF <cherifmohamedabraham@gmail.com>
2024-06-23 03:12:48 +0000

    Update Filter Array.js

--- Array/Filter Array.js
+++ Array/Filter Array.js
@@ -1,4 +1,3 @@
-// Déclaration de la fonction filtreTableau en utilisant une fonction fléchée
 const filtreTableau = (tableau) => {
     // Utilisation de la méthode filter pour créer un nouveau tableau contenant uniquement les nombres pairs
     const tableauPair = tableau.filter(nombre => nombre % 2 === 0);
@@ -6,6 +5,5 @@ const filtreTableau = (tableau) => {
     // Retourne une chaîne de caractères qui inclut les nombres pairs du tableau
     return `Les nombres pairs sont : ${tableauPair}`;
 };
-
 //Exemple :
 console.log(filtreTableau([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
